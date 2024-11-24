49d3154e5fa744a8db605a80c0ab86ea621fefc1
maloryware <mal.larsson.reyes@gmail.com>
2024-11-24 22:56:13 +0000

    defaults or whtev

--- consts.py
+++ consts.py
@@ -1,4 +1,5 @@
 class Identifiers:
     core_service = "4138"
-    config = "/config.js"
-    backup_config = "backup_config.js"
+    config = "./config.js"
+    backup_config = "./backup_config.js"
+    default_config = "./default_config.js"
