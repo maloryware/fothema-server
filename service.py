3c923974d56da53dc2a4630565645b67a6280cc9
maloryware <mal.larsson.reyes@gmail.com>
2024-11-25 22:25:36 +0000

    offset check

--- service.py
+++ service.py
@@ -1,5 +1,6 @@
+from typing import override
 from bluez_peripheral.gatt.service import Service
-from bluez_peripheral.gatt.characteristic import characteristic, CharacteristicFlags as CharFlags
+from bluez_peripheral.gatt.characteristic import characteristic, CharacteristicFlags as CharFlags, CharacteristicReadOptions as ReadOptions, CharacteristicWriteOptions as WriteOptions
 from bluez_peripheral.gatt.descriptor import descriptor, DescriptorFlags as DescFlags
 from fileman import Config
 import json
@@ -16,29 +17,29 @@ class MirrorServ(Service):
         pass
     
     @characteristic("413A", CharFlags.READ)
-    def ping(self):
+    def ping(self, options):
         toSend = bytes("pong!")
         print(f"Ping function called! Sending {toSend}")
         return bytes("pong!", "utf-8")
 
     @characteristic("413B", CharFlags.READ)
-    def send(self, arg):
-        print(f"2nd argument: {arg}")
+    def send(self, options):
         toSend = bytes(json.dumps(Config.read()), "utf-8")
-        print(f"Sending config")
+        # print(f"Sending config: {toSend}")
+        print(f"sending config with offset={options.offset}")
         return toSend
     
     @characteristic("413C", CharFlags.WRITE)
-    def receive(self, config):
+    def receive(self, config, options):
         Config.write(config)
         print("Updating config")
 
     @characteristic("413D", CharFlags.WRITE)
-    def backup(self):
+    def backup(self, options):
         Config.write(Config.read, Identifiers.backup_config)
     
     @characteristic("413E", CharFlags.READ)
-    def connect(self, deviceInfo):
+    def connect(self, deviceInfo, options):
         print(f"device connected, info: {deviceInfo}")
 
     @characteristic("4140", CharFlags.READ)
