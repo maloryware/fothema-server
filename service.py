36806b714a005385a1a1af481718d82d85a50284
maloryware <mal.larsson.reyes@gmail.com>
2024-11-25 04:06:29 +0000

    readded debug prints...

--- service.py
+++ service.py
@@ -23,14 +23,15 @@ class MirrorServ(Service):
 
     @characteristic("413B", CharFlags.READ)
     def send(self, arg):
-        # print(f"2nd argument: {arg}")
+        print(f"2nd argument: {arg}")
         toSend = bytes(json.dumps(Config.read()), "utf-8")
-        # print(f"Send function called! Sending {toSend}")
+        print(f"Sending config")
         return toSend
     
     @characteristic("413C", CharFlags.WRITE)
     def receive(self, config):
         Config.write(config)
+        print("Updating config")
 
     @characteristic("413D", CharFlags.WRITE)
     def backup(self):
@@ -38,4 +39,8 @@ class MirrorServ(Service):
     
     @characteristic("413E", CharFlags.READ)
     def connect(self, deviceInfo):
-        print(f"device connected! info: {deviceInfo}")
+        print(f"device connected, info: {deviceInfo}")
+
+    @characteristic("4140", CharFlags.READ)
+    def resetDefault(self, options):
+        print("Resetting config to default [UNIMPLEMENTED]")
