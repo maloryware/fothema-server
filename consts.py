e4a9ada605221b7e2b447e3de89cccbf4504af0d
maloryware <mal.larsson.reyes@gmail.com>
2024-12-12 23:09:03 +0000

    changed default config location

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 class Identifiers:
     core_service = "4138"
-    #config = "./config.js"
+    #config = "~/MagicMirror/config.js"
     config = "./temp_cfg.js"
     buf = "./buffer_cfg.js"
     backup_config = "./backup_config.js"
