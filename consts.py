cc36417077bc25c0a77495899375b75ee5559b29
maloryware <mal.larsson.reyes@gmail.com>
2024-12-15 20:38:16 +0000

    bgfdggfd

--- consts.py
+++ consts.py
@@ -1,7 +1,7 @@
 class Identifiers:
     core_service = "4138"
     #config = "~/MagicMirror/config/config.js"
-    config = "./temp_cfg.js"
+    config = "./config.js"
     buf = "./buffer_cfg.js"
     backup_config = "./backup_config.js"
     default_config = "./default_config.js"
